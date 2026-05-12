c6320351f3b51d67dff4862a1473a1959d3d47f8
Halil Durak <halildrk@gmail.com>
2026-05-12 18:16:29 +0300

    changes for chrome driver

--- driver/driver_chrome.go
+++ driver/driver_chrome.go
@@ -15,31 +15,19 @@ type Chrome struct {
 
 // InitChrome initializes a Chrome driver.
 func InitChrome(proxy string) *Chrome {
-	//wd, err := os.Getwd()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
 	opts := append(
 		chromedp.DefaultExecAllocatorOptions[:],
-		//chromedp.UserDataDir(os.TempDir()+"/chrome-tmp"),
 		chromedp.ProxyServer(proxy),
 		chromedp.Flag("disable-web-security", true),
 		chromedp.Flag("ignore-certificate-errors", true),
 		chromedp.UserAgent("Chrome"),
-		//chromedp.Flag("remote-debugging-port", "8080"),
 		chromedp.Flag("enable-logging", true),
-		chromedp.Flag("v", "0"),
-		//chromedp.UserDataDir(wd),
-		//chromedp.Flag("log-level", "0"),
+		chromedp.Flag("v", "1"),
+		chromedp.UserDataDir("/tmp/pandaria/chrome-userdata-dir"),
 		chromedp.WindowSize(1200, 800),
 		chromedp.NoSandbox,
+		//chromedp.Flag("remote-debugging-port", "8080"),
 	)
-
-	//f, err := os.Create("chrome-log.txt")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
 
 	allocatorCtx, allocatorCancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	cdpCtx, cdpCancel := chromedp.NewContext(allocatorCtx)
