a72e9c65c93c9577eba452b5bc3fda13fcdfcf03
Halil Durak <halildrk@gmail.com>
2026-05-14 22:57:49 +0300

    move CLI related stuff to `cli` package

---
+++ cli/cli.go
@@ -0,0 +1,45 @@
+package cli
+
+type FetchOptions struct {
+	URL             string `arg:"" help:"Where to fetch."`
+	HTTPProxyAddr   string `help:"Address of HTTP proxy server." default:"127.0.0.1:3000"`
+	LPAddr          string `help:"Address of Lightpanda CDP server." default:"127.0.0.1:9222"`
+	EnableHTTPProxy bool   `help:"Whether run an HTTP proxy server." default:"true"`
+	EnableLP        bool   `help:"Whether connect to a Lightpanda instance." default:"true"`
+	EnableChrome    bool   `help:"Whether run a Chrome instance." default:"true"`
+	ChromeHeadless  bool   `help:"Whether run Chrome in a headless mode." default:"true"`
+}
+
+type Commands struct {
+	Fetch FetchOptions `cmd:"" help: "Fetch from a URL."`
+}
+
+var CLI Commands
+
+func URL() string {
+	return CLI.Fetch.URL
+}
+
+func HTTPProxyAddr() string {
+	return CLI.Fetch.HTTPProxyAddr
+}
+
+func LPAddr() string {
+	return CLI.Fetch.LPAddr
+}
+
+func EnableHTTPProxy() bool {
+	return CLI.Fetch.EnableHTTPProxy
+}
+
+func EnableLP() bool {
+	return CLI.Fetch.EnableLP
+}
+
+func EnableChrome() bool {
+	return CLI.Fetch.EnableChrome
+}
+
+func ChromeHeadless() bool {
+	return CLI.Fetch.ChromeHeadless
+}
